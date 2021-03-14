08bb1da2ba2eba6a46ed5d6ac6182dcd2059b47a
colevs <colervs@gmail.com>
2021-03-13 16:17:47 -0800

    Limited data to stolen values under 000000

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -6,7 +6,7 @@ def load_and_process(path_to_csv):
     df = (
     	pd.read_csv(path_to_csv)
     	.loc[lambda x: x['STOLEN_VALUE']>1000]
-        .loc[lambda x: x['STOLEN_VALUE']<100000]
+        .loc[lambda x: x['STOLEN_VALUE']<1000000]
         .drop(['ORI', 'PUB_AGENCY_NAME', 'PUB_AGENCY_UNIT', 'AGENCY_TYPE_NAME', 'STATE_ABBR', 'POPULATION_GROUP_CODE', 'OFFENSE_CODE', 'VICTIM_TYPE_CODE', 'LOCATION_CODE', 'WEAPON_CODE', 'PROP_DESC_ID', 'PROP_DESC_CODE'], axis=1)
    		.sort_values("DATA_YEAR", ascending = True)
    		.rename(columns={
